435cd5dcf36ed881788169520d83c16832c9432b
Celox16 <sck04184@naver.com>
2020-11-24 23:32:59 +0900

    edit 11-24 11:32

--- OS_TermProject_test/src/schedule_algorithm/Input.java
+++ OS_TermProject_test/src/schedule_algorithm/Input.java
@@ -5,55 +5,44 @@ import java.awt.Container;
 import javax.swing.*;
 
 public class Input extends JFrame{
-	private JTextField[] TextInputPID;
-	private JTextField[] TextInputArrivalTime;
-	private JTextField[] TextInputRunningTime;
 	
-	private int ProcessCount, TimeSlice;
+	
 	
 	public Input(int ProcessCount, int TimeSlice) {
-		this.ProcessCount = ProcessCount;
-		this.TimeSlice = TimeSlice;
 		
 		setTitle("Input");
-		setSize(420,500);
+		setSize(450,500);
 		setVisible(true);
 		
 		Container c = getContentPane(); // 자유로운 layout
 		c.setLayout(null);				// 자유로운 layout
 		
-		JLabel LabelPID = new JLabel("PID 입력");
-		JLabel LabelArrivalTime = new JLabel("도착시간 입력");
-		JLabel LabelRunningTime = new JLabel("실행시간 입력");
+		JLabel LabelPID = new JLabel("프로세스아이디");
+		JLabel LabelArrivalTime = new JLabel("도착시간");
+		JLabel LabelRunningTime = new JLabel("실행시간");
+		JLabel LabelPriority = new JLabel("우선순위");
 		
-		JButton ButtonInputComplete = new JButton("입력 완료");
+		JButton InputComplete = new JButton("입력완료");
 		
-		TextInputPID = new JTextField[this.ProcessCount];
-		//TextInputArrivalTime = new JTextField[this.ProcessCount];
-		//TextInputRunningTime = new JTextField[this.ProcessCount];
+		LabelPID.setBounds(10, 10, 100, 20);
+		LabelArrivalTime.setBounds(120, 10, 60, 20);
+		LabelRunningTime.setBounds(190, 10, 60, 20);
+		LabelPriority.setBounds(260, 10, 60, 20);
 		
-		
-		for(int i=0;i<this.ProcessCount;i++) {
-			TextInputPID[i].setBounds(10, 20+10*i, 60, 60);
-			getContentPane().add(TextInputPID[i]);
-			TextInputPID[i].setColumns(10);
-			
-		}
+		InputComplete.setBounds(340, 10, 90, 20);
 		
 		
-		LabelPID.setBounds(10, 10, 60, 15);				//왼쪽 시작 px, 위에 시작 px, 길이, 높이
-		LabelArrivalTime.setBounds(90, 10, 100, 15);
-		LabelRunningTime.setBounds(200, 10, 100, 15);
-		
-		ButtonInputComplete.setBounds(300, 10, 100, 15);
-		
-		
-		getContentPane().add(LabelPID);
+		getContentPane().add(LabelPID);	
 		getContentPane().add(LabelArrivalTime);
 		getContentPane().add(LabelRunningTime);
+		getContentPane().add(LabelPriority);
+		
+		getContentPane().add(InputComplete);
+		
+		JTextField[] textField = new JTextField[1];
+		textField[1].setBounds(10, 51, 116, 21);
+		getContentPane().add(textField[1]);
+		textField[1].setColumns(10);
 				
-		getContentPane().add(ButtonInputComplete);
-		//Hello
-		//gfyhgfy
 	}
 }
