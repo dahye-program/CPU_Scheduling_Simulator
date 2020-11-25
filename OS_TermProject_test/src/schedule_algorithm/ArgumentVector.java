f7a0412685fba38e3dd2219ef60e9134a15b5a0f
park <park@DESKTOP-FDKOLR6>
2020-11-25 22:50:28 +0900

    edit argumentvector 1125_1050

--- OS_TermProject_test/src/schedule_algorithm/ArgumentVector.java
+++ OS_TermProject_test/src/schedule_algorithm/ArgumentVector.java
@@ -4,11 +4,7 @@ public class ArgumentVector {
 	int ArrivalTime;
 	int RunningTime;
 	String PID;
-	public ArgumentVector(int ArrivalTime, int RunningTime, String PID) {
-		this.ArrivalTime = ArrivalTime;
-		this.RunningTime = RunningTime;
-		this.PID = PID;
-	}
+	
 	public void FixArgument(int ArrivalTime, int RunningTime, String PID) {
 		this.ArrivalTime = ArrivalTime;
 		this.RunningTime = RunningTime;
