8ed3b9423f8fa1b6e774a51955f9e1a33b4ce77b
park <park@DESKTOP-FDKOLR6>
2020-11-26 00:40:16 +0900

    edit 1126_1239 ArrivalTime -> StartTime

--- OS_TermProject_test/src/schedule_algorithm/ArgumentVector.java
+++ OS_TermProject_test/src/schedule_algorithm/ArgumentVector.java
@@ -1,13 +1,31 @@
 package schedule_algorithm;
 
 public class ArgumentVector {
-	int ArrivalTime;
+	int StartTime;
 	int RunningTime;
 	String PID;
 	
-	public void FixArgument(int ArrivalTime, int RunningTime, String PID) {
-		this.ArrivalTime = ArrivalTime;
+	public void FixArgument(int StartTime, int RunningTime, String PID) {
+		this.StartTime = StartTime;
 		this.RunningTime = RunningTime;
 		this.PID = PID;
+	}
+	
+	void ReSet(int StartTime, int RunningTime, String PID) {
+		this.StartTime = StartTime;
+		this.RunningTime = RunningTime;
+		this.PID = PID;
+	}
+	
+	int ReturnArrivalTime() {
+		return StartTime;
+	}
+	
+	int ReturnRunningTime() {
+		return RunningTime;
+	}
+	
+	String ReturnPID() {
+		return PID;
 	}
 }
